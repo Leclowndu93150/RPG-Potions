074be903c5e5e92b104e2c6c3a39b2ad637ed929
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-10-28 21:58:04 +0100

    ok good

--- src/main/java/com/leclowndu93150/rpg_potions/client/BlackStainOverlay.java
+++ src/main/java/com/leclowndu93150/rpg_potions/client/BlackStainOverlay.java
@@ -3,11 +3,8 @@ package com.leclowndu93150.rpg_potions.client;
 import com.leclowndu93150.rpg_potions.RPGPotions;
 import com.leclowndu93150.rpg_potions.config.PotionConfig;
 import com.leclowndu93150.rpg_potions.init.ModEffects;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -16,8 +13,6 @@ import net.neoforged.neoforge.client.event.RenderGuiLayerEvent;
 
 @EventBusSubscriber(modid = RPGPotions.MODID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
 public class BlackStainOverlay {
-    
-    private static final ResourceLocation BLACK_STAIN_TEXTURE = ResourceLocation.fromNamespaceAndPath(RPGPotions.MODID, "textures/misc/black_stain.png");
     
     @SubscribeEvent
     public static void onRenderGui(RenderGuiLayerEvent.Post event) {
@@ -31,15 +26,8 @@ public class BlackStainOverlay {
             
             int intensity = PotionConfig.BLACK_STAIN_INTENSITY.get();
             float alpha = intensity / 100.0f;
-            
-            RenderSystem.enableBlend();
-            RenderSystem.defaultBlendFunc();
-            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha);
-            
-            guiGraphics.blit(BLACK_STAIN_TEXTURE, 0, 0, 0, 0, screenWidth, screenHeight, screenWidth, screenHeight);
-            
-            RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-            RenderSystem.disableBlend();
+            int darkColor = (int)(alpha * 255) << 24;
+            guiGraphics.fill(0, 0, screenWidth, screenHeight, darkColor);
         }
     }
 }
