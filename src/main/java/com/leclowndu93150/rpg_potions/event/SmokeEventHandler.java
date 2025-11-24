c53b53f9a0569078fe31a2c141d19e93e7468b6b
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-11-24 20:22:33 +0100

    fixes

--- src/main/java/com/leclowndu93150/rpg_potions/event/SmokeEventHandler.java
+++ src/main/java/com/leclowndu93150/rpg_potions/event/SmokeEventHandler.java
@@ -9,9 +9,14 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.phys.HitResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SmokeEventHandler {
     
@@ -24,42 +29,55 @@ public class SmokeEventHandler {
             }
         });
     }
-    
+
     public static boolean onPotionImpact(ThrownPotion thrownPotion, HitResult hitResult) {
         if (!(thrownPotion.level() instanceof ServerLevel serverLevel)) {
             return true;
         }
-        
-        Potion potion = PotionUtils.getPotion(thrownPotion.getItem());
-        if (potion == null) {
-            return true;
-        }
-        
-        boolean hasSmokeEffect = false;
-        int duration = 200;
-        
-        for (MobEffectInstance effect : potion.getEffects()) {
+
+        ItemStack potionStack = thrownPotion.getItem();
+        List<MobEffectInstance> allEffects = PotionUtils.getMobEffects(potionStack);
+
+        MobEffectInstance smokeEffect = null;
+        List<MobEffectInstance> remainingEffects = new ArrayList<>();
+
+        for (MobEffectInstance effect : allEffects) {
             if (effect.getEffect() == ModEffects.SMOKE) {
-                hasSmokeEffect = true;
-                duration = effect.getDuration();
-                break;
+                smokeEffect = effect;
+            } else {
+                remainingEffects.add(effect);
             }
         }
-        
-        if (hasSmokeEffect) {
-            SmokeEmitterEntity emitter = new SmokeEmitterEntity(
-                    serverLevel,
-                    hitResult.getLocation().x,
-                    hitResult.getLocation().y,
-                    hitResult.getLocation().z,
-                    duration
-            );
-            serverLevel.addFreshEntity(emitter);
-            
+
+        if (smokeEffect == null) {
+            return true;
+        }
+
+        SmokeEmitterEntity emitter = new SmokeEmitterEntity(
+                serverLevel,
+                hitResult.getLocation().x,
+                hitResult.getLocation().y,
+                hitResult.getLocation().z,
+                smokeEffect.getDuration()
+        );
+        serverLevel.addFreshEntity(emitter);
+
+        if (remainingEffects.isEmpty()) {
             thrownPotion.discard();
             return false;
+        } else {
+            ItemStack newStack = new ItemStack(potionStack.getItem());
+            if (potionStack.hasTag()) {
+                newStack.setTag(potionStack.getTag().copy());
+            }
+            PotionUtils.setPotion(newStack, Potions.WATER);
+            PotionUtils.setCustomEffects(newStack, remainingEffects);
+            if (potionStack.hasCustomHoverName()) {
+                newStack.setHoverName(potionStack.getHoverName());
+            }
+            thrownPotion.setItem(newStack);
+            return true;
         }
-        return true;
     }
     
     private static void onLivingTick(LivingEntity entity, ServerLevel serverLevel) {
