6fcfd1d82c10bc76bbfa324cf3d7cc816083dda5
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-11-24 20:14:24 +0100

    fix

--- src/main/java/com/leclowndu93150/rpg_potions/event/SmokeEventHandler.java
+++ src/main/java/com/leclowndu93150/rpg_potions/event/SmokeEventHandler.java
@@ -8,57 +8,74 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.phys.HitResult;
 import net.minecraftforge.event.entity.ProjectileImpactEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.List;
+
 import static com.leclowndu93150.rpg_potions.RPGPotions.MODID;
 
 @Mod.EventBusSubscriber(modid = MODID)
 public class SmokeEventHandler {
-    
+
     @SubscribeEvent
     public static void onPotionImpact(ProjectileImpactEvent event) {
         if (!(event.getProjectile() instanceof ThrownPotion thrownPotion)) {
             return;
         }
-        
+
         if (!(event.getProjectile().level() instanceof ServerLevel serverLevel)) {
             return;
         }
-        
-        net.minecraft.world.item.alchemy.Potion potion = net.minecraft.world.item.alchemy.PotionUtils.getPotion(thrownPotion.getItem());
-        if (potion == null) {
-            return;
-        }
-        
-        boolean hasSmokeEffect = false;
-        int duration = 200;
-        
-        for (MobEffectInstance effect : potion.getEffects()) {
+
+        ItemStack potionStack = thrownPotion.getItem();
+        List<MobEffectInstance> allEffects = PotionUtils.getMobEffects(potionStack);
+
+        MobEffectInstance smokeEffect = null;
+        for (MobEffectInstance effect : allEffects) {
             if (effect.getEffect() == ModEffects.SMOKE.get()) {
-                hasSmokeEffect = true;
-                duration = effect.getDuration();
+                smokeEffect = effect;
                 break;
             }
         }
-        
-        if (hasSmokeEffect) {
-            HitResult hitResult = event.getRayTraceResult();
-            
-            SmokeEmitterEntity emitter = new SmokeEmitterEntity(
-                    serverLevel,
-                    hitResult.getLocation().x,
-                    hitResult.getLocation().y,
-                    hitResult.getLocation().z,
-                    duration
-            );
-            serverLevel.addFreshEntity(emitter);
-            
+
+        if (smokeEffect == null) {
+            return;
+        }
+
+        HitResult hitResult = event.getRayTraceResult();
+        SmokeEmitterEntity emitter = new SmokeEmitterEntity(
+                serverLevel,
+                hitResult.getLocation().x,
+                hitResult.getLocation().y,
+                hitResult.getLocation().z,
+                smokeEffect.getDuration()
+        );
+        serverLevel.addFreshEntity(emitter);
+
+        List<MobEffectInstance> remainingEffects = allEffects.stream()
+                .filter(e -> e.getEffect() != ModEffects.SMOKE.get())
+                .toList();
+
+        if (remainingEffects.isEmpty()) {
             event.setCanceled(true);
             thrownPotion.discard();
+        } else {
+            ItemStack newStack = new ItemStack(potionStack.getItem());
+            newStack.setTag(potionStack.getTag() != null ? potionStack.getTag().copy() : null);
+            PotionUtils.setPotion(newStack, Potions.WATER);
+            PotionUtils.setCustomEffects(newStack, remainingEffects);
+            if (potionStack.hasCustomHoverName()) {
+                newStack.setHoverName(potionStack.getHoverName());
+            }
+            thrownPotion.setItem(newStack);
         }
     }
     
